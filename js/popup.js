939d19e43710aadbfcd4a7a8a8bb7b856b9597b5
root <root@Vathsan.localdomain>
2020-05-19 18:49:27 +0530

    Beautification

--- js/popup.js
+++ js/popup.js
@@ -5,9 +5,10 @@ function enableDarkMode(){
         var toggler = document.getElementById('toggle');
 
         toggler.addEventListener('click',function(){
-            chrome.tabs.executeScript({
-                file : 'js/dark.js'
-            });
+            chrome.tabs.executeScript( null, { file : 'js/jquery.min.js'}, function() {
+                    chrome.tabs.executeScript( null, { file : 'js/dark.js'});
+                }
+            );
         });
     });
 }
